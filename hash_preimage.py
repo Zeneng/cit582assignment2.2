8ecd595f64457cb2fdd7e777a8c912d3ea3d2e9f
ZENENG FAN ZENENG FAN <fanzen@seas.upenn.edu>
2021-05-23 22:13:54 +0000

    add function content

--- hash_preimage.py
+++ hash_preimage.py
@@ -1,11 +1,30 @@
 import hashlib
-import os
+import string
+import random
 
 def hash_preimage(target_string):
+    if not isinstance(target_string, str):
+        print( "Input need to be a string" )
+        return
+        
     if not all( [x in '01' for x in target_string ] ):
         print( "Input should be a string of bits" )
         return
-    nonce = b'\x00'
+    
+    len(target_string) = k
+    if k > 256:
+        print( "Input is too long" )
+        return
+    
+    notEqual = True
+    end_length = 256
+    while(notEqual):
+        x = ''.join(random.choices(string.ascii_uppercase + string.digits, k = 50)).encode('utf-8')
+        hx = hashlib.sha256(x)
+        hx16 = hx.hexdigest()
+        hx2 = bin(int(hx16, 16))[2:].zfill(end_length)
+        hx_trailing = hx2[-k:]
+        if hx_trailing == target_string:
+            notEqual = False
 
-    return( nonce )
-
+    return( x )
